1efdbd4606c09523fe12c0c18b630467fd20fb6d
HangHang <dhkdwlsgod@naver.com>
2022-12-05 15:11:04 +0900

    1205

--- 유형별/문자열/-4949/4949.py
+++ 유형별/문자열/-4949/4949.py
@@ -1,14 +1,30 @@
 import sys
-input = sys.stdin.readline
+# input = sys.stdin.readline
 
 while 1:
     a= input()
-    if a ==".":
+
+    if a == ".":
         break
     kk = []
-
+    team=1
     for i in a:
-        if i =="(" or i =="[":
+        if i == "(" or i == "[":
             kk.append(i)
-        elif i =="]":
+        elif i == "]":
+            if kk and kk[-1]=="[":
+                kk.pop()
+            else:
+                team =0
+                break
+        elif i == ")":
+            if kk and  kk[-1] == "(":
+                kk.pop()
+            else:
+                team =0
+                break
 
+    if team==1 and len(kk)==0:
+        print("yes")
+    else:
+        print("no")
