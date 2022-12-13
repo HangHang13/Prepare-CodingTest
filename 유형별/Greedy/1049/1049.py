3b2cf1f9495a6fa4139d04f5097307b61927325d
{HangHang13} <dhkdwlsgod@naver.com>
2022-12-13 10:09:08 +0900

    1213

--- 유형별/Greedy/1049/1049.py
+++ 유형별/Greedy/1049/1049.py
@@ -10,19 +10,10 @@ con= 0
 _min=9999999
 kim=0
 _minTax = min(lst, key=lambda x:x[0])
-# print(type(_minTax))
-# print(_minTax[0])
 _minPay = min(lst, key=lambda x:x[1])
-# print(_minPay[1])
-
-
-
 sixkim = int(n//6)
 zero = int(n%6)
-
-
 _min = min(_min, _minTax[0]*sixkim+_minPay[1]*zero, _minPay[1]*n, _minTax[0]*(sixkim+1) )
 
-# _min = min(_min, _minTax[0], _minPay[1]*n)
 
 print(_min)
