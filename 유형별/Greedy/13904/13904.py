d59853b0f3d6b4881b8ae20d1d67cde993d6cffd
{HangHang13} <dhkdwlsgod@naver.com>
2022-12-29 18:36:57 +0900

    1229

--- 유형별/Greedy/13904/13904.py
+++ 유형별/Greedy/13904/13904.py
@@ -1,47 +1,28 @@
 import sys
-
-from queue import PriorityQueue
-
 # input = sys.stdin.readline
 sys.stdin = open("input.txt", "rt")
 
 n = int(input())
-q = PriorityQueue(maxsize=n)
+
 arr = []
-_max = 0
 for _ in range(n):
     a, b = map(int, input().split())
     arr.append((a, b))
-
-    q.put((a, b))
-
-_max = max(arr, key=lambda x: x[1])[1]
-arr.sort(key=lambda x: (x[0], x[-1]))
-print(arr)
-
+# arr.sort(key=lambda x: x[1], reverse=True)
+arr.sort()
 cnt = 0
+vis = [False] * (1000 + 1)
+print(arr)
+do=[]
+first = arr[-1][0]
+while True:
+    if first == 0:
+        break
+    while arr and arr[-1][0] >=first:
+        do.append(arr.pop()[1])
+    first -=1
+    if not do:
+        continue
+    print(do)
 
-vis = [False] * (n + 1)
-
-
-def get_index(a, arr):
-    ans = 0
-    for x, y in arr:
-        ans += 1
-        if y == a:
-            return ans - 1
-        else:
-            continue
-
-
-for i in range(len(arr)):
-    print(arr[i][1])
-    if arr[i][1] >= _max:
-        cnt += arr[i][1]
-    else:
-        cnt += _max
-        idx = get_index(_max, arr)
-        arr.pop(idx)
-        _max = max(arr, key=lambda x: x[1])[1]
-
-print(cnt)
+print(sum(do))
---
+++ 유형별/구현/21608/21608.py
@@ -0,0 +1,28 @@
+import sys
+import collections
+
+sys.stdin = open('input.txt', 'r')
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
+q = collections.deque()
+n = int(input())
+std = [list(map(int, input().split())) for _ in range(n)]
+grid = [[0] for _ in range(n)]
+arr = []
+
+print(grid)
+
+vis = [[False] for _ in range(n)]
+
+
+def bfs(num):
+    while q:
+        y, x = q.popleft()
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0 <= ny < n and 0 <= nx < n and not vis[ny][nx] and (abs(ny - y) + abs(nx - x)) == 1:
+                vis[ny][nx] = num
+                q.append((ny, nx))
+            else:
+                continue
