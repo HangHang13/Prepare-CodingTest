0e5e8a8b9e3f2aaf308276e22c4f2b75cfcd5ad0
{HangHang13} <dhkdwlsgod@naver.com>
2023-01-24 21:56:21 +0900

    ppap

--- BOJ/-16120/ppap.py
+++ BOJ/-16120/ppap.py
@@ -1,3 +1,19 @@
-lst = list(input().rstrip())
+ppap = input()
+p = ['P', 'P','A','P']
 
-print(lst)
+if ppap == 'P' or ppap == 'PPAP':
+    print('PPAP')
+else:
+    stack = []
+    for i in ppap:
+        stack.append(i)
+        if stack[-4:] == p:
+            stack.pop()
+            stack.pop()
+            stack.pop()
+
+
+    if stack == ppap or stack ==['P']:
+        print('PPAP')
+    else:
+        print('NP')
