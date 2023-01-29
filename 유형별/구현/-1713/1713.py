64788931d00db9cb3264611fe56b624277f30ad1
{HangHang13} <dhkdwlsgod@naver.com>
2023-01-29 19:04:06 +0900

    0129

--- 유형별/구현/-1713/1713.py
+++ 유형별/구현/-1713/1713.py
@@ -1,36 +1,24 @@
-n=int(input())
-a=int(input())
-import collections
-lst= collections.deque([])
-lst = list(map(int,input().split()))
-pic = []
-print(lst)
-cha = [0]*a
+n = int(input())  ##사진틀 갯수
+vote = int(input())  ## 총 추천 횟수
+student = list(map(int, input().split()))  # 추천학생 번호
+pic = []  # 사진틀
+score = []  # 사진틀과 매치하여 추천수 저장할 리스트
 
-while lst:
-    t = lst.pop(0)
-    cha[t-1]+=1
-    pic.insert(0,t)
-    if len(pic) >= n:
-        aa = min(cha)
-        _min_list = []
-        for idx in range(len(cha)):
-            if cha[idx] == aa:
-                _min_list.append(cha[idx])
-                if len(_min_list) > 3:
-                    pic.pop()
+for i in range((vote)):
+    if student[i] in pic:
+        for j in range(len(pic)):
+            if student[i] == pic[j]:
+                score[j] += 1
+    else:
+        if len(pic) >= n:
+            for j in range(n):
+                if score[j] == min(score):
+                    del pic[j]
+                    del score[j]
                     break
 
-    print(pic)
+        pic.append(student[i])
+        score.append(1)
 
-
-
-
-
-al =[0,0,1,1]
-
-print(min(al))
-
-
-
-
+pic.sort()
+print(' '.join(map(str, pic)))
