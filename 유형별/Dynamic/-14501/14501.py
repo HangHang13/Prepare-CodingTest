a39d2a179525b40e69aa637a2bb3c17a791a7897
{HangHang13} <dhkdwlsgod@naver.com>
2023-02-08 00:19:29 +0900

    0208

--- 유형별/Dynamic/-14501/14501.py
+++ 유형별/Dynamic/-14501/14501.py
@@ -1,12 +1,25 @@
 n=int(input())
 t=[]
 p=[]
-dp=[]
+dp=[0 for i in range(n+1)]
 for _ in range(n):
    a,b = map(int, input().split())
    t.append(a)
    p.append(b)
-   dp.append(b)
 
 
-print(lst)
+
+
+for i in range(n):
+   for j in range(i+t[i], n+1):
+      if dp[j]<dp[i] + p[i]:
+         dp[j] = dp[i] + p[i]
+   print(dp)
+print("=====================")
+for i in range(n-1, -1,-1):
+   if i+t[i] > n:
+      dp[i] = dp[i+1]
+   else:
+      dp[i] = max(dp[i+1], p[i]+dp[i])
+
+   print(dp)
